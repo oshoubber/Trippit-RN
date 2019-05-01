12476ade4584df72fe9c0bf57ff18a72759b2a37
Co <a17nguyen@scu.edu>
2019-05-01 00:54:22 -0700

    fixed TSP. works properly on console, need to put on UI

--- screens/ListsScreen.js
+++ screens/ListsScreen.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {
   Image,
@@ -13,10 +14,11 @@ import {
 let results;
 let resultsLength;
 let final_res = Number.MAX_VALUE;
-let final_path;
+
+//get distance from one point to another
 function getDistance(start, end){
   return new Promise(function(resolve, reject){
-    console.log("promise getDistance");
+    //console.log("getting Distance");
     var distance = require('react-native-google-matrix');
       distance.get(
         {
@@ -30,205 +32,15 @@ function getDistance(start, end){
         //console.log("results length: " + results.length);
         //console.log(results);
         resultsLength = results.length;
-        resolve(data);
+        resolve(data); 
         
       });
     });
 }
 
-function Path(path, cost, utility, matrix){
-  this.path = path;
-  this.cost = cost;
-  this.utility = utility; 
-  this.matrix = matrix;
-  this.nullFlag = false;
-}
-
-function Perms(length){
-  this.perms = Array(length + 1).fill(0);
-}
-
-function swap(perms1, index1, index2){
-  var temp = perms1.perms[index1];
-  perms1.perms[index1] = perms1.perms[index2];
-  perms1.perms[index2] = temp;
-}
-
-function run(matrix, places){
-  var p = new Path([], Number.MAX_VALUE, 0, matrix);
-  var perms = new Perms(places.length);
-  init_perms(perms);
-  permutate(perms, 1, places.length - 1, p);
-  return p;
-}
-
-function init_perms(perms){
-  for(var i = 0; i < perms.perms.length - 1; i++){
-    perms.perms[i] = i;
-  }
-  perms.perms[perms.length - 1] = 0;
-}
-
-function permutate(perms, begin, end, p){
-  if(begin == end){
-    update_if_better(perms, p);
-  } else{
-    for(var i = begin; i <= end; i++){
-      swap(perms, begin, i);
-      permutate(perms, begin + 1, end, p);
-      swap(perms, begin, i);
-    }
-  }
-}
-
-function update_if_better(perms, p){
-  var updated_cost = get_cost(perms, p);
-  if(updated_cost < p.cost){
-    p.path = perms.perms;
-    p.cost = updated_cost;
-  }
-}
-
-function get_cost(perms, p){
-  var acc_cost = 0;
-  for(var i = 0; i + 1 < perms.perms.length;++i){
-    acc_cost += p.matrix[perms.perms[i]][perms.perms[i+1]];
-  }
-  return acc_cost;
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-//replace later for efficiency
-function searchForIndex(address, addresses){
-  for(var i = 0; i < addresses.length; i++){
-    if(addresses[i] == address) return i;
-  }
-  return 0;
-}
-
-function uniformDistribute(start, end, places) {
-  var data = []
-  var a;
-  for (var i = 0; i < places.length; i++){
-    a = getRandomInt(start, end);
-    data.push(a);
-  }
-  return data;
-}
-
-//O(n)
-function union(path, vertex){
-  var P = path;
-  for(var i = 0; i < path.path.length; i++){
-    if(path.path[i] == vertex)
-      return path;
-  }
-  P.path.push(vertex);
-  return P;
-}
 
 export default class ListsScreen extends React.Component {
   static navigationOptions = { header: null, };
-
-
-//get distance from one point to another
-
-
-greedyTSP(places, addresses){
-  return new Promise(function(resolve, reject){
-    var utilityArray = uniformDistribute(1800, 3660, places);
-    console.log("promise greedyTSP");
-    function utility(P){
-      var u = 0;
-      for(var i = 0; i < P.path.length; i++){
-        u += utilityArray[searchForIndex(P.path[i], addresses)];
-      }
-      return u;
-    }
-    var empty = [[],[]];
-    var P_ = new Path(addresses, 0, 0, empty);
-
-
-    while(P_.nullFlag != true){
-        var P = P_;
-        var best_margin = 0;
-        P_.nullFlag = true;
-        for(var i = 0; i < places.length; i++){
-          var currentpath = union(P, addresses[i]);
-          var promise = getDistance(currentpath.path, currentpath.path);
-          promise.then(function(result){
-            //console.log(result);
-              let durationMatrix = [];
-              let currentRow = [];
-              for(var j = 0; j < results.length; j += places.length){
-                //console.log(results[j]);
-                currentRow = results.slice(j, j + places.length);
-                durationMatrix.push(currentRow);
-              }
-              for(var i = 0; i < durationMatrix.length; i++){
-                for(var j = 0; j < durationMatrix.length; j++){
-                  durationMatrix[i][j] = parseInt(durationMatrix[i][j].durationValue);
-                }
-              }
-              //console.log("Duration Matrix length: ", durationMatrix.length);
-              //console.log("Duration Matrix: ");
-             // console.log(durationMatrix);
-              
-              var path = run(durationMatrix, places);
-              //console.log("pathcost ", path.cost);
-              //console.log("P cost ", P.cost);
-              //console.log("utility path, ", utility(path));
-              //console.log("utility P, ", utility(P));
-              //console.log(path.matrix);
-              var margin = (utility(path) - utility(P)) / (path.cost - P.cost);
-              //console.log("margin", margin);
-              //implement budget later
-              if(margin > best_margin){
-                best_margin = margin;
-                P_ = path;
-                final_path = P;
-                console.log("still in loop", P_);
-                P_.nullFlag = false;
-              } 
-              /*
-              console.log("Minimum cost", path.cost);
-              console.log("Path taken: ");
-              console.log(path.path);
-              for(var i = 0; i < path.path.length; i++){
-                console.log(places[path.path[i]].key);
-              } */
-            
-          }, function(err) {
-            console.log(err);
-          });
-        }
-      }
-      resolve(P_);
-        
-  });
-}
-
- getTimes(P_){
-   return new Promise(function(resolve, reject){
-    console.log("promise get Times");
-    let durationMatrix = P_.matrix;
-    var time_start = 0;
-    var time_to_vertex = 0;
-    var time_list = [];
-    for(var i = 0; i < P_.path - 1; i++){
-      time_to_vertex = time_start + utilityArray[P_.path[i]] + durationMatrix[P_.path[i]][P_.path[i + 1]];
-      //time_to_vertex /= 60;
-      var time = [time_start, time_to_vertex];
-      time_list.push(time);
-      time_start = time_to_vertex;
-    }
-    resolve(P_);
-   });
-  
-}
 
   render() {
     // Get all data from SearchScreen
@@ -238,20 +50,266 @@ greedyTSP(places, addresses){
     //console.log(places);
     //console.log(addresses);
     //console.log(hours);
+    var utilityArray = uniformDistribute(1800, 3660, places);
 
+    function utility(P, places, addresses){
+      var u = 0;
+      for(var i = 0; i < P.path.length; i++){
+        u += utilityArray[searchForIndex(P.path[i], addresses)];
+      }
+      return u;
+    }
+
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    function searchForIndex(address, addresses){
+      for(var i = 0; i < addresses.length; i++){
+        if(addresses[i] == address) return i;
+      }
+      return 0;
+    }
+
+    function linearsearch(P, element){
+      return new Promise((resolve) => {
+        for(var i = 0; i < P.path.length; i++){
+          if(P.path[i] == element) resolve(true);
+        }
+        resolve(false);
+      });
+    }
     
+    function uniformDistribute(start, end, places) {
+      var data = []
+      var a;
+      for (var i = 0; i < places.length; i++){
+        a = getRandomInt(start, end);
+        data.push(a);
+      }
+      return data;
+    }
     
+    //O(n)
+    function union(path, vertex){
+      return new Promise(function(resolve, reject){
+        var P = path;
+        P.path.push(vertex);
+        resolve(P);
+      });
+    } 
+
+    function createMatrix(data, currentpath){
+      return new Promise(function(resolve, reject){
+        //console.log("creating matrix");
+        let durationMatrix = [];
+        let currentRow = [];
+        for(var j = 0; j < data.length; j += currentpath.path.length){
+          //console.log(results[j]);
+          currentRow = data.slice(j, j + currentpath.path.length);
+          durationMatrix.push(currentRow);
+        }
+        for(var i = 0; i < durationMatrix.length; i++){
+          for(var j = 0; j < durationMatrix[0].length; j++){
+            durationMatrix[i][j] = parseInt(durationMatrix[i][j].durationValue);
+          }
+        }
+        resolve(durationMatrix);
+      });
+    }
+
+    function greedyTSP(places, addresses){
+      return new Promise(function(resolve){
+//console.log("greedyTSP");
+      var empty = [[],[]];
+      var initial = addresses[0];
+      var startingarray = [];
+      startingarray.push(initial);
+      var P_ = new Path(startingarray, 0, 0, empty);
+
+      var copyofaddress = addresses;
+      var promise = greedyTSPHelper(P_, places, copyofaddress);
+      promise.then((result) =>{
+          //console.log("final result: ", result);
+          var time_start = 0;
+          var time_to_vertex = 0;
+          var time_list = [];
+          for(var i = 0; i < result.path.length - 1; i++){
+            time_to_vertex = time_start + result.matrix[result.path[i]][result.path[i + 1]];
+            //console.log("computing");
+            var time = [];
+            time.push(time_start);
+            time.push(time_to_vertex);
+            time_list.push(time);
+            var time_spent = time_to_vertex + utilityArray[result.path[i]];
+            var time2 = [];
+            time2.push(time_to_vertex);
+            time2.push(time_spent);
+            if(i == result.path.length - 2) break;
+            time_list.push(time2);
+            time_start = time_spent;
+          }
+          //console.log("time intervals:", time_list);
+          resolve([result, time_list])
+      });
+      });
+      
+    }
+
+    function greedyTSPHelper(P_, places, addresses){
+      return new Promise(function(resolve, reject){
+        //console.log("TSP helper");
+        var P = P_;
+        var best_margin = 0;
+        P_.nullFlag = true;
+        var promise = forLoop(P, best_margin, P_, places, addresses);
+        promise.then((result) =>{
+          resolve(result);
+        });
+      });
+    }
+
+    function forLoop(P, best_margin, P_, places, addresses){
+        return new Promise((resolve) =>{
+          var requests = 0;
+          for(let value of addresses){  
+            ++requests;
+            var b = linearsearch(P, value);
+            b.then((found) =>{
+              if(found){return};
+              //console.log("iterating");
+              var p = union(P, value);
+              p.then((joined) =>{
+                var currentpath = joined;
+                var promise = getDistance(currentpath.path, currentpath.path);
+                promise.then(
+                (result)=>{
+                  var promise2 = createMatrix(result, currentpath);
+                  promise2.then((durationMatrix) => {
+                    //console.log(durationMatrix);
+                    var path = run(durationMatrix, currentpath);//run TSP
+                    path.then((p) =>{
+                      var margin = (utility(p, places, addresses) - utility(P, places, addresses)) / (p.cost - P.cost);
+                      if(margin > best_margin){
+                        best_margin = margin;
+                        P_ = p;
+                        P_.utility = utility(p, places, addresses);
+                        P_.matrix = durationMatrix;
+                        //console.log("still in loop", P_);
+                        P_.nullFlag = false;
+                      }
+                      if(requests == addresses.length) resolve(P_);
+                    });
+                  });
+                  });//promise
+              });
+            });    
+          }
+        });
+
+    }
+
+
+    function Path(path, cost, utility, matrix){
+      this.path = path;
+      this.cost = cost;
+      this.utility = utility; 
+      this.matrix = matrix;
+      this.nullFlag = false;
+    }
+
+    function Perms(length){
+      this.perms = Array(length - 1).fill(0);
+    }
+
+    function run(matrix, currentpath){
+      return new Promise(resolve => {
+        //console.log("run");
+        var p = new Path([], Number.MAX_VALUE, 0, matrix);
+        var perms = new Perms(currentpath.path.length);
+        var promise = init_perms(perms);
+        promise.then((result) =>{
+          var promise2 = permutate(perms);
+          promise2.then((list) =>{
+            for(var i = 0; i < list.length; i++){
+              list[i].push(0);
+              list[i].unshift(0);
+              var promise3 = update_if_better(list[i], p);
+            }
+            resolve(p);
+          });
+        });
+      });
+      
+    }
+
+    function init_perms(perms){
+      return new Promise(resolve =>{
+        for(var i = 0; i < perms.perms.length; i++){
+          perms.perms[i] = i + 1;
+        }
+        resolve(perms);
+      });
+    }
+
+
+    function permutate(perms){
+      //console.log("permutating");
+      return new Promise(resolve => {
+        var nums = perms.perms;
+        let result = {};
+        result[0] = [[nums[0]]];
+        for (let i = 1; i < nums.length; i++) {
+          let prev = result[i - 1];
+          for (let prevItem of prev) {
+              for (let j = 0; j <= prevItem.length; j++) {
+                  let cur = prevItem.slice();
+                  cur.splice(j, 0, nums[i]);
+                  if (!result[i]) {
+                      result[i] = [];
+                  }
+                  result[i].push(cur);
+              }
+          }
+        }
+        resolve(result[nums.length - 1]);
+      });
+    }
+
+    function update_if_better(perms, p){
+      return new Promise(resolve => {
+        var promise = get_cost(perms, p);
+        promise.then((acc_cost) =>{
+          if(acc_cost < p.cost){
+            p.path = perms;
+            p.cost = acc_cost;
+          }
+          resolve(p);
+        });
+      });    
+    }
+
+    function get_cost(perms, p){
+      return new Promise(resolve => {
+        var acc_cost = 0;
+        for(var i = 0; i + 1 < perms.length;++i){
+          acc_cost += p.matrix[perms[i]][perms[i+1]];
+        }
+        resolve(acc_cost);
+      });
+    }
 
     
     if (places != null) {
 
       //GreedyTSPCost
       if(places.length > 1){
-        this.greedyTSP(places, addresses).then(
-          (P_) => {
-            console.log("Final: ", P_);
-  
-          });
+        var finalTSP = greedyTSP(places, addresses);
+        finalTSP.then((result) =>{
+          console.log("Final path is: ", result[0]);
+          console.log("Final time intervals are: ", result[1]);
+        });
+       
       }  
 
       return ( // Return list if array is not empty
@@ -270,9 +328,8 @@ greedyTSP(places, addresses){
         </View>
       );
     }
-  }//render
-}//export
-
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
